04edc204ed231fb678f62431764e2f35887ede5c
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2025-04-27 15:16:02 +0200

    Put up nightshaded variants

--- js/gallery-handler.js
+++ js/gallery-handler.js
@@ -1,34 +1,35 @@
+galleryFolder = "media/gallery-ns/";
 gallery = [];
-gallery_titles = [];
-gallery_size = 0;
-gallery_used = [];
-gallery_overlay_visible = false;
-bg_list = [];
+galleryTitles = [];
+gallerySize = 0;
+galleryUsed = [];
+galleryOverlayVisible = false;
+bgList = [];
 
 // Attempts to load the gallery from the dedicated JSON file
 function load_gallery(_callback) {
-  $.getJSON("media/gallery/gallery.json", function (data) {
+  $.getJSON(galleryFolder + "gallery.json", function (data) {
     gallery.length = 0;
-    gallery_used.length = 0;
-    gallery_size = 0;
+    galleryUsed.length = 0;
+    gallerySize = 0;
     $.each(data, function(key, val) {
-      var url = "media/gallery/" + key;
+      var url = galleryFolder + key;
       gallery.push(url);
-	  gallery_titles.push(val);
-      gallery_size++;
+	  galleryTitles.push(val);
+      gallerySize++;
     });
 
     $.getJSON("media/wallpapers/wallpapers.json", function (data) {
-      bg_list.length = 0;
+      bgList.length = 0;
       $.each(data, function(key, val) {
         var url = "media/wallpapers/" + val;
-        bg_list.push(url);
+        bgList.push(url);
       });
 
       load_bg_random();
     });
 
-    for (let i = 0; i < gallery_size; i++) gallery_used.push(false);
+    for (let i = 0; i < gallerySize; i++) galleryUsed.push(false);
     for (f of _callback) f();
   });
 }
@@ -40,7 +41,7 @@ function populate_gallery() {
   while (line1.hasChildNodes()) line1.removeChild(line1.firstChild);
   while (line2.hasChildNodes()) line2.removeChild(line2.firstChild);
 
-  for (let i = 0; i < gallery_size; i++) {
+  for (let i = 0; i < gallerySize; i++) {
     var img_next = fetch_next();
 //    img_next += "?t=" + (new Date().getTime());
     var title = img_next.title;
@@ -100,8 +101,8 @@ function stretch_gallery() {
 
 // FIXME Look into why the first fadein is instantaneous
 function gallery_overlay_fade_in(img_id) {
-  if (!gallery_overlay_visible) {
-    gallery_overlay_visible = true;
+  if (!galleryOverlayVisible) {
+    galleryOverlayVisible = true;
     gallery_overlay_change(img_id);
     document.getElementById("gallery-overlay").style.visibility = "visible";
     $("#gallery-overlay").fadeIn(250, function() { document.getElementById("gallery-overlay").style.visibility = "visible"; });
@@ -113,7 +114,7 @@ function gallery_overlay_change(img_id) {
   img.src = img_id;
 
   var title = document.getElementById("gallery-title");
-  var text = gallery_titles[gallery.indexOf(img_id)] + "<br/>";
+  var text = galleryTitles[gallery.indexOf(img_id)] + "<br/>";
 
   var first = Array.from(img_id)[14];
   switch (first) {
@@ -130,11 +131,11 @@ function gallery_overlay_change(img_id) {
 function gallery_overlay_change_prev() {
   var i;
   var img_id = document.getElementById("gallery-overlay-img").getAttribute("src").split('?')[0];
-  for (i = 0; i < gallery_size; i++) {
+  for (i = 0; i < gallerySize; i++) {
     if (gallery[i] == img_id) break;
   }
 
-  if (i == 0 || i >= gallery_size) return;
+  if (i == 0 || i >= gallerySize) return;
   $("#gallery-overlay-img").fadeOut(250, function() {
     gallery_overlay_change(gallery[i - 1]);
     $("#gallery-overlay-img").fadeIn(250);
@@ -144,11 +145,11 @@ function gallery_overlay_change_prev() {
 function gallery_overlay_change_next() {
   var i;
   var img_id = document.getElementById("gallery-overlay-img").getAttribute("src").split('?')[0];
-  for (i = 0; i < gallery_size; i++) {
+  for (i = 0; i < gallerySize; i++) {
     if (gallery[i] == img_id) break;
   }
 
-  if (i >= gallery_size - 1) return;
+  if (i >= gallerySize - 1) return;
   $("#gallery-overlay-img").fadeOut(250, function() {
     gallery_overlay_change(gallery[i + 1]);
     $("#gallery-overlay-img").fadeIn(250);
@@ -156,8 +157,8 @@ function gallery_overlay_change_next() {
 }
 
 function gallery_overlay_fade_out() {
-  if (!gallery_overlay_visible) return;
-  gallery_overlay_visible = false;
+  if (!galleryOverlayVisible) return;
+  galleryOverlayVisible = false;
   $("#gallery-overlay").fadeOut(250, function() { document.getElementById("gallery-overlay").style.visibility = "hidden"; });
 }
 
@@ -179,10 +180,10 @@ function populate_img_fields_random() {
 
 // Gets the URL to the next unused element of the gallery, returning a placeholder error if no images are available
 function fetch_next() {
-  for (let i = 0; i < gallery_size; i++) {
-    if (!gallery_used[i]) {
-      gallery_used[i] = true;
-      return { "url": gallery[i], "title": gallery_titles[i] };
+  for (let i = 0; i < gallerySize; i++) {
+    if (!galleryUsed[i]) {
+      galleryUsed[i] = true;
+      return { "url": gallery[i], "title": galleryTitles[i] };
     }
   }
   return { "url": "media/oops.png", "title": "An error occured loading this image" };
@@ -193,30 +194,30 @@ function fetch_random() {
   // Checking if there are any free pictures left, returning an error if not
   // TODO Figure out a better system for this, probably by popping already-used elements
   var ctr = 0;
-  for (let i = 0; i < gallery_size; i++) {
-    if (gallery_used[i]) ctr++;
+  for (let i = 0; i < gallerySize; i++) {
+    if (galleryUsed[i]) ctr++;
   }
-  if (ctr >= gallery_size) return { "url": "media/oops.png", "title": "An error occured loading this image" };
+  if (ctr >= gallerySize) return { "url": "media/oops.png", "title": "An error occured loading this image" };
 
   // Tries to get a random element of the gallery, trying again if its choice is already being unused
   // TODO Figure out a better system for this, probably by popping already-used elements
   while (1) {
-    var r = Math.floor(Math.random() * gallery_size);
-    if (!gallery_used[r]) {
-      gallery_used[r] = true;
-      return { "url": gallery[r], "title": gallery_titles[r] };
+    var r = Math.floor(Math.random() * gallerySize);
+    if (!galleryUsed[r]) {
+      galleryUsed[r] = true;
+      return { "url": gallery[r], "title": galleryTitles[r] };
     }
   }
 }
 
 // Gets the URL to a random element of the gallery, returning a placeholder error if no images are available
 function fetch_bg_random() {
-  if (bg_list.length <= 0) return "media/oops.png";
+  if (bgList.length <= 0) return "media/oops.png";
 
-  var r = Math.floor(Math.random() * bg_list.length);
-  return bg_list[r];
+  var r = Math.floor(Math.random() * bgList.length);
+  return bgList[r];
 }
 
 function clear_used_list() {
-  for (let i = 0; i < gallery_size; i++) gallery_used[i] = false;
+  for (let i = 0; i < gallerySize; i++) galleryUsed[i] = false;
 }
